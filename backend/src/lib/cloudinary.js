c659555990107c5230c1e32c56ac065d4e5ae7c8
lakshmiganesh0925 <96818262+lakshmiganesh0925@users.noreply.github.com>
2025-01-02 15:19:22 +0530

    Update cloudinary.js

--- backend/src/lib/cloudinary.js
+++ backend/src/lib/cloudinary.js
@@ -1,5 +1,7 @@
-const cloudinary = require("cloudinary").v2;
-require('dotenv').config();
+import {v2 as cloudinary} from "cloudinary";
+import {config} from "dotenv";
+  
+config();
 
 cloudinary.config({
   cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
@@ -7,7 +9,7 @@ cloudinary.config({
   api_secret:process.env.CLOUDINARY_API_SECRET,
 });
 
-module.exports=cloudinary;
+export default cloudinary;
 
 /*const cloudinary = require('cloudinary').v2; // Ensure .v2 is used
 
