a4a963aca03304abf60cf5450c7acb7cbf1021b6
lakshmiganesh0925 <96818262+lakshmiganesh0925@users.noreply.github.com>
2025-01-02 15:41:31 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,10 @@ import express from 'express';
 import dotenv from "dotenv"
 import cookieParse  from "cookie-parser"
 import cors from "cors"
-import  authRoutes  from './routes/authRoutes'
-import  messageRoutes from "./routes/messageRoutes"
-import { connectDB } from './lib/db';
-import  {app,server}  from "./lib/socket";
+import  authRoutes  from './routes/authRoutes.js'
+import  messageRoutes from "./routes/messageRoutes.js"
+import { connectDB } from './lib/db.js';
+import  {app,server}  from "./lib/socket.js";
 import  path  from "path";
 
 
